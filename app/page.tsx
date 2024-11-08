03470620c2f41f4f01969ec90de1a0bbb6629f0c
Shrikanth <shrikantha.ma@juegostudioz.com>
2024-11-08 15:39:26 +0530

    added properties page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperites from "@/components/HomeProperties";
 const Homepage= () => {
     return ( 
         <>
           <Hero />
           <InfoBoxes />
+          <HomeProperites />
         </>
     );
 }
