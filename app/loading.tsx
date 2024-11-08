6bff12c4809d328f35a8372b2229964dcafb74ff
Shrikanth <shrikantha.ma@juegostudioz.com>
2024-11-08 16:57:52 +0530

    added loading page and not found page

---
+++ app/loading.tsx
@@ -0,0 +1,19 @@
+import { ClipLoader } from "react-spinners";
+const LoadingPage = () => {
+    const override = {
+        display: "block",
+        margin: "100px auto",
+    }
+    return (
+        <div>
+            <ClipLoader 
+              color="#3b82f6" 
+              cssOverride={override} 
+              size={150} 
+              aria-label="Loading Spinner" 
+            />
+        </div>
+    );
+}
+
+export default LoadingPage;
